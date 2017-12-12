0b40104b1acd42e04b3ea30109df56b4e578a0c4
pcASUS_grey\esnho <esnhoesnho@gmail.com>
2017-12-12 23:04:17 +0100

    Added a comment system to the index.js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,23 @@
+/* import THREE library along with a simple css used
+ * to eliminate margins on the page
+ */
 import * as THREE from 'three';
-//import App from './app.js';
 require('../css/style.css');
 
+/* in this index file I will accumulate files created
+ * during Milano Live Code threeJS lessons
+ * decomment blocks of code to visualize the relative creation
+ * at the bottom of the file lives a not very complex app used as experiment
+ */
+
+// decomment this to use the 006 lesson's code
 //require('./006-scene-essentials.js');
-require('./007-populate-the-scene.js');
 
-/*const app = new App();
+// decomment this to use the 007 lesson's code
+//require('./007-populate-the-scene.js');
 
+// Decomment these lines to start a not very complex app
+/*import App from './app.js';
+const app = new App();
 app.addCube();
 app.startRender();*/
