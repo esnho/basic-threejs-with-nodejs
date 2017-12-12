7e6d654a48ac8dc51de16e445b4116f315f50c0d
pcASUS_grey\esnho <esnhoesnho@gmail.com>
2017-12-12 20:48:32 +0100

    Added 006 Scene Essentials Code - closed in function

--- src/js/006-scene-essentials.js
+++ src/js/006-scene-essentials.js
@@ -1,18 +1,19 @@
 import * as THREE from 'three';
 
-var scene = new THREE.Scene();
+function init() {
+  var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth/window.innerHeight,
-  1,
-  1000
-);
+  var camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth/window.innerHeight,
+    1,
+    1000
+  );
 
-var renderer = new THREE.WebGLRenderer();
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild( renderer.domElement );
+  renderer.render(scene, camera);
+}
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild( renderer.domElement );
-
-renderer.render(scene, camera);
+init();
